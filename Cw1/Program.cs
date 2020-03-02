54fc514a60836fafe6a63ae3f769b4e59cbd4e59
Michał Warchał <s18358@pjwstk.edu.pl>
2020-03-02 15:29:04 +0100

    First Commit

---
+++ Cw1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+
+            var newperson = new Person { FirstName = "Michal" };
+
+            var url = "https://www.pja.edu.pl/en/";
+            var httpClinet = new HttpClient();
+            var response = await httpClinet.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var htmlContent = await response.Content.ReadAsStringAsync();
+                var regx = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+",RegexOptions.IgnoreCase);
+                var matches = regx.Matches(htmlContent);
+
+                foreach (var match in matches)
+                {
+                    Console.WriteLine(match.ToString());
+                }
+            }
+        }
+    }
+}
